fecaa72df225a402c55ab7f4e74b0ca669359c55
kent <kentvan21@gmail.com>
2023-07-25 21:03:48 -0500

    first commit

---
+++ src/main/java/kentv/shopSimulatorBE/ShopSimulatorBE.java
@@ -0,0 +1,21 @@
+package kentv.shopSimulatorBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ShopSimulatorBE {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSimulatorBE.class, args);
+	}
+
+	@GetMapping("/greet")
+	public String greet() {
+		return "Hello, World!";
+	}
+
+}
---
+++ src/test/java/kentv/shopSimulatorBE/ShopSimulatorBETests.java
@@ -0,0 +1,13 @@
+package kentv.shopSimulatorBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSimulatorBETests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
