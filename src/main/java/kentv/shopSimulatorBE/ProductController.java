0f7f178d59aa58cd3126bea310ea78691e47834a
kent <kentvan21@gmail.com>
2023-07-27 21:04:34 -0500

    Created Product class and controller to send back a sample list

---
+++ src/main/java/kentv/shopSimulatorBE/ProductController.java
@@ -0,0 +1,27 @@
+package kentv.shopSimulatorBE;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products")
+    public List<Product> getProduct() {
+        List<Product> productList = new ArrayList<>();
+
+        Product GPU = new Product("GPU", 349.99);
+        Product car = new Product("Car", 5999.99);
+        Product controller = new Product("Controller", 69.99);
+
+        productList.add(GPU);
+        productList.add(car);
+        productList.add(controller);
+
+        return productList;
+    }
+
+}
