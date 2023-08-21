7b4a08ac3f00b10f545c3b94e5d5b70baa797989
kent <kentvan21@gmail.com>
2023-08-20 20:02:19 -0500

    fixed forbidden error when sending POST request

--- src/main/java/kentv/shopSimulatorBE/Config/SecurityConfig.java
+++ src/main/java/kentv/shopSimulatorBE/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package kentv.shopSimulatorBE.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,9 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .anyRequest().permitAll()
-                );
+                )
+                .formLogin(Customizer.withDefaults())
+                .csrf((csrf) -> csrf.disable()); // not recommended but needed to disable for post request in from react
 
         return http.build();
     }
